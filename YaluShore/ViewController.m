1d20839e138f201feb689f9f218a51999b7b4e46
Michael D. Vinci <michaeldvinci@gmail.com>
2015-11-02 21:04:37 -0500

    Need to figure out how to emulate terminal [Return Key] entry

--- YaluShore/ViewController.m
+++ YaluShore/ViewController.m
@@ -12,11 +12,12 @@
 @synthesize outputArea, outputArea2;
 @synthesize labelText;
 
+int step = 1;
+
 - (void)viewDidLoad {
+    step = 1;
     [super viewDidLoad];
     [labelText setStringValue: @"Starting"];
-    
-    
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -24,45 +25,49 @@
 }
 
 - (IBAction)jailbreakIt:(NSButton *)sender {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
-    NSTask *task;
-    task = [[NSTask alloc] init];
-    [task setLaunchPath: @"/bin/sh"];
-    
-    NSArray *arguments;
-    NSString* newpath = [[NSBundle mainBundle] pathForResource:@"yalu-master/run" ofType:@"sh"];
-    NSLog(@"shell script path: %@",newpath);
-    arguments = [NSArray arrayWithObjects:newpath, nil];
-    [task setArguments: arguments];
-    
-    NSPipe *pipe;
-    pipe = [NSPipe pipe];
-    NSPipe *input = [NSPipe pipe];
-    [task setStandardOutput: pipe];
-    [task setStandardInput: input];
-    
-    NSFileHandle *file;
-    file = [pipe fileHandleForReading];
-    [task launch];
-    
-    NSData *data;
-    data = [file readDataToEndOfFile];
-    
-    NSString *string;
-    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    if (step == 1) {
         
-        NSString *string2 = string;
+        [labelText setStringValue: @"Running in background, sit tight!"];
         
-        dispatch_async(dispatch_get_main_queue(), ^(void){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
+            NSTask *task;
+            task = [[NSTask alloc] init];
+            [task setLaunchPath: @"/bin/sh"];
+    
+            NSArray *arguments;
+            NSString* newpath = [[NSBundle mainBundle] pathForResource:@"yalu-master/run" ofType:@"sh"];
+            NSLog(@"shell script path: %@",newpath);
+            arguments = [NSArray arrayWithObjects:newpath, nil];
+            [task setArguments: arguments];
+    
+            NSPipe *pipe;
+            pipe = [NSPipe pipe];
+            [task setStandardOutput: pipe];
+    
+            NSFileHandle *file;
+            file = [pipe fileHandleForReading];
+        
+            [task launch];
+    
+            NSData *data;
+            data = [file readDataToEndOfFile];
+    
+            NSString *string;
+            string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+ 
+            NSLog(@"string = %@",string);
+        
+            dispatch_async(dispatch_get_main_queue(), ^(void){
             
-            [labelText setStringValue: string2];
-        }
-
-    );
+                [labelText setStringValue: string];
+            } );
         
-        //NSLog (@"script returned:\n%@", string);
+        } );
+        
+        [labelText setStringValue: @"When device has restarted, press button!"];
+        step = 2;
     }
-);}
+}
 
 
 @end
